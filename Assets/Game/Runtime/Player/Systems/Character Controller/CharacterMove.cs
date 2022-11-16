9131eaaf7d80aeb4a3f87d33853177bb63e1b1dd
BartKnucle <sjarde@yahoo.fr>
2022-11-16 16:16:10 +0100

    Add basic movements

--- Assets/Game/Runtime/Player/Systems/Character Controller/CharacterMove.cs
+++ Assets/Game/Runtime/Player/Systems/Character Controller/CharacterMove.cs
@@ -1,14 +1,27 @@
 using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using UnityEngine;
 
 namespace Game.Player.Systems
 {
+    [UpdateInGroup(typeof(CharacterControllerSystemGroup))]
     public partial class CharacterMove : SystemBase
     {
+        PlayerInputs inputActions;
+        protected override void OnCreate()
+        {
+            inputActions = new PlayerInputs();
+            inputActions.Enable();
+        }
         protected override void OnUpdate()
         {
-            Entities.ForEach(() =>
+            Entities.ForEach((ref Translation translation, in Player.Components.Tags.Player player ) =>
             {
-            }).Run();
+                translation.Value += (float3)inputActions.PlayerMovements.Move.ReadValue<Vector3>() * 100 * World.Time.DeltaTime;
+            })
+            .WithoutBurst()
+            .Run();
         }
     }
 }
